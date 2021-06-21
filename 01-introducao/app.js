f32621b29ecd9b48986333860f37f81c3102ad28
marcelopoars <marcelo.fatecpoa@gmail.com>
2021-06-21 16:05:39 -0300

    Trabalhando com promises e async / await

--- 01-introducao/app.js
+++ 01-introducao/app.js
@@ -9,39 +9,62 @@ function p(content) {
   - await é bloqueantes
 */
 
-
-// Usando promise
+// Exemplo 01 - Usando promise
 function getPosts() {
-  fetch('https://jsonplaceholder.typicode.com/posts')
-  .then((response) => response.json())
-  .then(console.log)
+  fetch("https://jsonplaceholder.typicode.com/posts") // retorna um readable string
+    .then((response) => response.json()) // converte para json / outra promise
+    .then((data) => console.log(data));
 }
 // getPosts()
 
-
-// Usando async / await
+// Exemplo 02 - Usando async / await
 async function getPosts2() {
   // aqui quero esperar o resultado do fetch chegar
-  const result = await fetch('https://jsonplaceholder.typicode.com/posts')
+  const result = await fetch("https://jsonplaceholder.typicode.com/posts");
   // Aqui o await trava a aplicação ate que a promise seja resolvida
-  return result.json()
+  return result.json(); // retorna uma promise
 }
 
-(async function() {
-  const posts = await getPosts2()
+// Usando uma função auto-invocável
+(async function () {
+  const posts = await getPosts2();
   // p(posts)
-}())
+})();
 
-// ou
-// getPosts2().then(console.log)
-
-
-
-//************************************
-// Exemplo 2
-async function getNumbers() {
-  return await 10
+// Exemplo 03 - Gerenciando vários requests
+// Vamos buscar todos usuário e seus posts
+async function getUsers() {
+  const result = await fetch("https://jsonplaceholder.typicode.com/users");
+  return result.json();
 }
 
-getPosts2().then(console.log)
-getNumbers().then(console.log)
+// 01 - Obtendo dados dos usuários - Usando 'then()'
+// getUsers().then(console.table)
+
+async function getUserPosts(userId) {
+  const posts = await fetch(
+    `https://jsonplaceholder.typicode.com/users/${userId}/posts`
+  );
+  return posts.json(); // Retorna uma promise
+}
+
+// 02 - Obtendo dados dos usuários - Usando uma função auto-invocável
+(async function () {
+  const users = await getUsers();
+  const posts = [];
+
+  for (let i = 0; i < users.length; i++) {
+    posts.push(getUserPosts(users[i].id));
+  }
+
+  const allPosts = await Promise.all(posts)
+  console.log(allPosts);
+})();
+
+// 03 - Obtendo dados dos usuários - Iterando com map()
+(async function () {
+  const users = await getUsers();
+  const result = await Promise.all(users.map(user => getUserPosts(user.id)))
+  console.log(result);
+})();
+
